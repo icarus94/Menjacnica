756d0240ee48b100f7920ca988c7171673b21a22
icarus94 <icarusrevolution88@gmail.com>
2015-03-31 02:59:38 +0200

    logička kontrola u svim set metodama privatnih atributa koji su uvedeni u klasi Valuta.

--- src/menjacnica/Valuta.java
+++ src/menjacnica/Valuta.java
@@ -11,36 +11,50 @@ public class Valuta {
 		return nazivValute;
 	}
 	public void setNazivValute(String nazivValute) {
+		if (nazivValute.length() == 0 || nazivValute.equals(null))
+			throw new RuntimeException("Nije unet naziv");
 		this.nazivValute = nazivValute;
 	}
 	public String getKracinazivValute() {
 		return kracinazivValute;
 	}
 	public void setKracinazivValute(String kracinazivValute) {
+		if (kracinazivValute.length() != 3)
+			throw new RuntimeException(
+					"Kraci naziv mora da sadrzi 3 slova");
 		this.kracinazivValute = kracinazivValute;
 	}
 	public double getKupovnaValuta() {
 		return kupovnaValuta;
 	}
 	public void setKupovnaValuta(double kupovnaValuta) {
+		if (kupovnaValuta <= 0)
+			throw new RuntimeException("Pogresno unet");
 		this.kupovnaValuta = kupovnaValuta;
 	}
 	public double getSrednjaValuta() {
 		return srednjaValuta;
 	}
 	public void setSrednjaValuta(double srednjaValuta) {
+		if (srednjaValuta <= 0)
+			throw new RuntimeException("Pogresno unet");
 		this.srednjaValuta = srednjaValuta;
 	}
 	public double getProdajnaValuta() {
 		return prodajnaValuta;
 	}
 	public void setProdajnaValuta(double prodajnaValuta) {
+		if (prodajnaValuta <= 0)
+			throw new RuntimeException("Pogresno unet");
 		this.prodajnaValuta = prodajnaValuta;
 	}
 	public GregorianCalendar getDatum() {
 		return datum;
 	}
-	public void setDatum(GregorianCalendar datum) {
+	public void setDatum(GregorianCalendar datum) throws Exception {
+		GregorianCalendar danas = new GregorianCalendar();
+		if(datum.after(danas))
+			throw new Exception("Ne moze buduci kurs biti unet,samo trenutni ili prosli");
 		this.datum = datum;
 	}
 	
