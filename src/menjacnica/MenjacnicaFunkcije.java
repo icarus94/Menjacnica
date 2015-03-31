cbe38ec8f6493fbc4af0bc59cdcdfd8cf139f259
icarus94 <icarusrevolution88@gmail.com>
2015-03-31 03:37:03 +0200

    Napravljena u master grani implementirane metode interfejsa u menjacnicafunkcije

--- src/menjacnica/MenjacnicaFunkcije.java
+++ src/menjacnica/MenjacnicaFunkcije.java
@@ -1,27 +1,50 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.specinterface.InterfaceMenjacnica;
 
 public class MenjacnicaFunkcije implements InterfaceMenjacnica {
 
+	LinkedList<Valuta> valuta = new LinkedList<>();
+	
 	@Override
 	public void dodajKurs(double prodajni, double kupovni,
 			GregorianCalendar dan, String naziv, String skraceniNazivValute) {
 		// TODO Auto-generated method stub
-		
+		Valuta v = new Valuta();
+		v.setSrednjaValuta((kupovni/2)+(prodajni/2));
+		v.setDatum(dan);
+		v.setProdajnaValuta(prodajni);
+		v.setKracinazivValute(skraceniNazivValute);
+		v.setKupovnaValuta(kupovni);
+		v.setNazivValute(naziv);
+		valuta.addFirst(v);
 	}
 
 	@Override
 	public void brisanjeKursa(GregorianCalendar dan, String skraceniNazivValute) {
 		// TODO Auto-generated method stub
-		
+		Valuta v = new Valuta();
+		v.setDatum(dan);
+		v.setKracinazivValute(skraceniNazivValute);
+		for(int i=0;i<valuta.size();i++){
+			if(valuta.get(i).equals(v))
+				valuta.remove(i);
+		}
 	}
 
 	@Override
 	public Valuta pronadjiKurs(GregorianCalendar dan, String skraceniNazivValute) {
 		// TODO Auto-generated method stub
+		Valuta v = new Valuta();
+		v.setDatum(dan);
+		v.setKracinazivValute(skraceniNazivValute);
+		for(int i=0;i<valuta.size();i++){
+			if(valuta.get(i).equals(v))
+				return valuta.get(i);
+		}
 		return null;
 	}
 	
